8b28bc1bedfc5aa897366f03a9ba5b264639f819
unknown <paul.mouzas@gmail.com>
2014-09-17 15:21:51 -0400

    add tech_paper_search app

---
+++ tech_papers_search/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var TechPaperApp = angular.module('TechPaperApp', ['ngSanitize']);
+
+TechPaperApp.controller('TechPaperListCtrl', function ($scope, $http) {
+  $http.get('data.json').success(function(data) {
+    $scope.papers = data;
+  });
+  $scope.isMammal = function(paper)
+  {
+    return paper.mammal == "";
+  };
+  
+});
